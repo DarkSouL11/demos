904bc215369082464550f76a45f7db0e13138734
DarkSouL11 <sundeep.sundeep11@gmail.com>
2019-08-03 09:42:06 +0000

    Updated github pages with react-template demo

---
+++ react-template/precache-manifest.3f1cacfdc9bc59204fa25e5ee1e17b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b9b0515924188c8dcb89c4a01d64bf",
+    "url": "/demos/react-template/index.html"
+  },
+  {
+    "revision": "f4b5540c291efcabb5f0",
+    "url": "/demos/react-template/static/css/main.20687760.chunk.css"
+  },
+  {
+    "revision": "8915697480e2667b2072",
+    "url": "/demos/react-template/static/js/2.49a007a5.chunk.js"
+  },
+  {
+    "revision": "f4b5540c291efcabb5f0",
+    "url": "/demos/react-template/static/js/main.fe141ae2.chunk.js"
+  },
+  {
+    "revision": "0ae8342f15f7b88e6d0f",
+    "url": "/demos/react-template/static/js/runtime~main.9476082b.js"
+  }
+]);
