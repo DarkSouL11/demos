05b11fad7d14731e58fb43ead00d92b787e7234c
DarkSouL11 <sundeep.sundeep11@gmail.com>
2019-08-04 11:08:44 +0000

    Updated github pages with tetris demo

---
+++ tetris/precache-manifest.36799a3a18ccc0b45ad7f90320f896e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28990e8cd4112c284877e6cae3bc9ae",
+    "url": "/demos/tetris/index.html"
+  },
+  {
+    "revision": "163a783ad6862e67396c",
+    "url": "/demos/tetris/static/css/main.29b94b63.chunk.css"
+  },
+  {
+    "revision": "8c98972888d0191e2d61",
+    "url": "/demos/tetris/static/js/2.f766fdcd.chunk.js"
+  },
+  {
+    "revision": "163a783ad6862e67396c",
+    "url": "/demos/tetris/static/js/main.3c10f9f6.chunk.js"
+  },
+  {
+    "revision": "1f302c2543730e7cfe41",
+    "url": "/demos/tetris/static/js/runtime~main.f3e786fb.js"
+  }
+]);
--- tetris/service-worker.js
+++ tetris/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demos/react-template/precache-manifest.3bc71ac39efb2874583fb238182f78cb.js"
+  "/demos/tetris/precache-manifest.36799a3a18ccc0b45ad7f90320f896e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demos/react-template/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demos/tetris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
